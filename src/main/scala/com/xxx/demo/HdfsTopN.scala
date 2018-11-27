4ab77eb4a895283c2abb2866966c56c2e236176d
xingang.jiang <xingang.jiang@cygia.com>
2018-11-27 14:15:16 +0800

    从HDFS里读数据进行TOP N

--- src/main/scala/com/xxx/demo/HdfsTopN.scala
+++ src/main/scala/com/xxx/demo/HdfsTopN.scala
@@ -1,18 +1,56 @@
+/**
+  * 第一：HDFS管理
+  * 进到hadoop:
+  * 启动
+  * ./sbin/start-dfs.sh
+  * 停止
+  *./sbin/stop-dfs.sh
+  *
+  * ./bin/hdfs dfs -mkdir -p /user/hadoop
+  * ./bin/hdfs dfs -mkdir input
+  * ./bin/hdfs dfs -put ./etc/hadoop/ *.xml input
+  * ./bin/hdfs dfs -ls input
+  * ./bin/hdfs dfs -cat output/ *
+  * ./bin/hdfs dfs -get output ./output     # 将 HDFS 上的 output 文件夹拷贝到本机
+  * ./bin/hdfs dfs -rm -r output    # 删除 output 文件夹
+  *
+  * 运行命令
+  * /usr/local/spark/bin/spark-submit --class "SimpleApp" ~/sparkapp/target/scala-2.11/simple-project_2.11-1.0.jar
+  * 上面命令执行后会输出太多信息，可以不使用上面命令，而使用下面命令查看想要的结果
+  *
+    18/11/27 14:07:49 DEBUG scheduler.DAGScheduler: After removal of stage 4, remaining stages = 1
+    18/11/27 14:07:49 DEBUG scheduler.DAGScheduler: After removal of stage 3, remaining stages = 0
+    18/11/27 14:07:49 INFO scheduler.DAGScheduler: Job 2 finished: take at HdfsTopN.scala:17, took 0.318734 s
+    1	7890
+    2	788
+    3	600
+    4	489
+    5	290
+    18/11/27 14:07:49 INFO spark.SparkContext: Invoking stop() from shutdown hook
+    18/11/27 14:07:49 DEBUG component.AbstractLifeCycle: stopping org.spark_project.jetty.server.Server@53967a9b
+  *
+  */
+
 package com.xxx.demo
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+  * 从HDFS读取数据进行排序
+  * @author jxg
+  */
 object HdfsTopN {
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("TopN").setMaster("local")
+    val conf = new SparkConf().setAppName("HdfsTopN").setMaster("local")
     val sc = new SparkContext(conf)
     sc.setLogLevel("DEBUG")
-    val lines = sc.textFile("hdfs://localhost:9000/user/hadoop/spark/mycode/rdd/examples", 2)
+
+    val lines = sc.textFile("hdfs://localhost:9000/user/hadoop/input", 2)
     var num = 0;
     val result = lines.filter(line => (line.trim().length > 0) && (line.split(",").length == 4))
       .map(_.split(",")(2))
-      .map(x => (x.toInt,""))
+      .map(x => (x.toInt, ""))
       .sortByKey(false)
       .map(x => x._1).take(5)
       .foreach(x => {
